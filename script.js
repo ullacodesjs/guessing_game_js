f789fc26674f933aa0b78fd806ba2f6c45b834ee
Ulla Angell <ullaangell15@gmail.com>
2022-02-13 13:15:41 +0000

    adding game logic

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 
 //DOM Manipulation
 
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Number!";
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Correct Number!";
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
 
-console.log(document.querySelector(".guess").value);
-document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+// document.querySelector(".guess").value = 23;
+
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
 
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = document.querySelector(".guess").value;
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "no number!";
