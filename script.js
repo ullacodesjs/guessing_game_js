4faaf297f2c872aeb6d4fbd0c5eb38ef807ae0d2
Ulla Angell <ullaangell15@gmail.com>
2022-02-13 19:13:58 +0000

    implementing highscore

--- script.js
+++ script.js
@@ -11,9 +11,9 @@
 // console.log(document.querySelector(".guess").value);
 // document.querySelector(".guess").value = 23;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-// document.querySelector(".number").textContent = secretNumber;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -31,6 +31,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
 
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
     //too low
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -53,4 +57,17 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".message").textContent = "start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "rgb(85, 34, 34)";
+  document.querySelector(".number").style.width = "15rem";
 });
