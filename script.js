051d437517bc12d3333a89885846e70bbf477620
Ulla Angell <ullaangell15@gmail.com>
2022-02-13 00:29:15 +0000

    selecting and manipulating elements

--- script.js
+++ script.js
@@ -4,3 +4,9 @@
 
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+console.log(document.querySelector(".guess").value);
+document.querySelector(".guess").value = 23;
