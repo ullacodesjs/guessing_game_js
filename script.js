9bcad22d9b3d940dbf74636728a527399099afcf
Ulla Angell <ullaangell15@gmail.com>
2022-02-13 14:24:13 +0000

    hid the secret number

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
+// document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -27,9 +27,8 @@ document.querySelector(".check").addEventListener("click", function () {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
-
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
-
     document.querySelector(".number").style.width = "30rem";
 
     //too low
