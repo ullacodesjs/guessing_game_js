3e23c797aff7136ab7ffccda12855c8a427fc833
Ulla Angell <ullaangell15@gmail.com>
2022-02-13 01:02:16 +0000

    handling click events

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ document.querySelector(".score").textContent = 10;
 
 console.log(document.querySelector(".guess").value);
 document.querySelector(".guess").value = 23;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "no number!";
+  }
+});
